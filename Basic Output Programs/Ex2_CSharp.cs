b31f9a1396bef2e931423ae473df5fd3dba629c9
Sean Berlin <seanberlin724@gmail.com>
2024-05-18 00:22:43 -0400

    Create Ex2_CSharp.cs

---
+++ Basic Output Programs/Ex2_CSharp.cs
@@ -0,0 +1,39 @@
+/******************Block Comment***********************
+ * Sean Berlin                          9-12-18
+ * This is a program that shows the basics of
+ * the c# program
+ * *****************************
+ *) Write a console application that displays the name of the programming language you are writing in.  One possible design is given here. Don’t forget your comments.
+
+			CCCCCCCCC                        #       #
+			C				#############
+			C				         #       #
+			C				        #       #
+			C			        ############
+			C				      #      #
+			CCCCCCCCC
+
+    */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("CCCCCCCCC              #   #");
+        Console.WriteLine("C                     #######");
+        Console.WriteLine("C                       #   #");
+        Console.WriteLine("C                       #   #");
+        Console.WriteLine("C                    ########");
+        Console.WriteLine("CCCCCCCCC              #    #");
+        Console.ReadLine();                                            
+        }
+    }
+}
