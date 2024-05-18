5c5cdb84fbe4ccefbd91833c9a13a0f327c96e87
Sean Berlin <seanberlin724@gmail.com>
2024-05-18 00:40:44 -0400

    Create Ex9_Initials.cs

---
+++ Basic Output Programs/Ex9_Initials.cs
@@ -0,0 +1,48 @@
+/******************Block Comment***********************
+* Sean Berlin
+* This is a program that shows the basics of
+* the c# program
+* 
+* Write your initials in block characters to the console.  Be creative.  
+* You may look up the unicode for more interesting characters 
+* than just the ones on your keyboard.
+* *****************************
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex9_Initials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "SB Initials";
+            Console.WriteLine("                        S S S S S S S                 BB                               ");
+            Console.WriteLine("                        S                             B                                ");
+            Console.WriteLine("                        S                             B                                ");
+            Console.WriteLine("                        S                             B                                ");
+            Console.WriteLine("                        S                             B                                ");
+            Console.WriteLine("                        S                             B                                ");
+            Console.WriteLine("                        S                             B                                ");
+            Console.WriteLine("                        S                             B                                ");
+            Console.WriteLine("                        S                             B                                ");
+            Console.WriteLine("                         S S S S S                    B                                ");
+            Console.WriteLine("                                  S                   B B B B B B B                    ");
+            Console.WriteLine("                                  S                   B           B                    ");
+            Console.WriteLine("                                  S                   B           B                    ");
+            Console.WriteLine("                                  S                   B           B                    ");
+            Console.WriteLine("                                  S                   B           B                    ");
+            Console.WriteLine("                                  S                   B           B                    ");
+            Console.WriteLine("                                  S                   B           B                    ");
+            Console.WriteLine("                                  S                   B           B                    ");
+            Console.WriteLine("                                  S                   B           B                    ");
+            Console.WriteLine("                    S S S S S S S S                   B B B B B B B                    ");
+            Console.ReadLine();
+        }
+    }
+}
