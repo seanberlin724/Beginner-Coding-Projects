05688e2197a501925a3c5b1ab9e407a65ccea66c
Sean Berlin <seanberlin724@gmail.com>
2024-05-18 00:37:59 -0400

    Create Ex8_BirthYear.cs

---
+++ Basic Output Programs/Ex8_BirthYear.cs
@@ -0,0 +1,28 @@
+/******************Block Comment***********************
+* Sean Berlin              Per.9            9-12-18
+* This is a program that shows the basics of
+* the c# program
+* 
+* Print your birth year in one column using one Console.
+* WriteLine statement.
+* *****************************
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex8_Birth_Year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "My Birthyear";
+            Console.WriteLine("2 \n0 \n0 \n1");
+            Console.ReadLine(); 
+        }
+    }
+}
