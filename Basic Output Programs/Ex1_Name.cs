16caa63aae89c62e03d2ae0fc78a13b0f487159e
Sean Berlin <seanberlin724@gmail.com>
2024-05-18 00:40:55 -0400

    Update Ex1_Name.cs

--- Basic Output Programs/Ex1_Name.cs
+++ Basic Output Programs/Ex1_Name.cs
@@ -1,5 +1,5 @@
 /******************Block Comment***********************
- * Sean Berlin          Per.9                9-12-18
+ * Sean Berlin
  * This is a program that shows the basics of
  * the c# program
  * *****************************/
