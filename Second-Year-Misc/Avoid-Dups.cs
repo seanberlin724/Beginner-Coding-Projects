5f665c702afc6be468cf7b95e506a37d3d3c875d
Sean Berlin <seanberlin724@gmail.com>
2024-05-22 18:22:38 -0400

    Update Avoid-Dups.cs

--- Second-Year-Misc/Avoid-Dups.cs
+++ Second-Year-Misc/Avoid-Dups.cs
@@ -1,18 +1,16 @@
 /******************Block Comment***********************
  * Sean Berlin
  * Program 415h (avoiding duplicates) 
-Program Description: 
-Use a single-subscripted array to solve the following problem. 
-Read in 20 numbers, each of which is between 10 and 100, inclusive.
-As each number is read, print it only if it is not a duplicate of a number already read.
-Provide for the "worst case" in which all 20 numbers are different 
-Use the smallest possible array to solve this problem. 
-Required Statements: input, output, loop control, decision making, array structure 
-Data file: P415h.txt 
-12 12 30 12 45 66 78 30 82 19 99 11 11 15 31 18 51 17 12 17 
-Sample output: 
-The original set of numbers are: 12 12 30 12 45 66 78 30 82 19 99 11 11 15 31 18 51 17 12 17 The different numbers from the set of integers are: 12 30 45 66 78 82 19 99 11 15 31 18 51 17 
-
+ * Program Description: Use a single-subscripted array to solve the following problem. 
+ * Read in 20 numbers, each of which is between 10 and 100, inclusive.
+ * As each number is read, print it only if it is not a duplicate of a number already read.
+ * Provide for the "worst case" in which all 20 numbers are different 
+ * Use the smallest possible array to solve this problem. 
+ * Required Statements: input, output, loop control, decision making, array structure 
+ * Data file: P415h.txt 
+ * Sample output: 
+ * The original set of numbers are: 12 12 30 12 45 66 78 30 82 19 99 11 11 15 31 18 51 17 12 17 
+ * The different numbers from the set of integers are: 12 30 45 66 78 82 19 99 11 15 31 18 51 17 
  * 
  * *****************************
  */
